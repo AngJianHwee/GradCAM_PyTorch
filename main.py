5e58fe62d864fff7aa381f152ec01ba7277f391e
AngJianHwee <angjianhwee@gmail.com>
2025-07-03 08:11:43 +0800

    Remove unnecessary sys.path modifications and clean up import statements

--- main.py
+++ main.py
@@ -5,14 +5,7 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
 
-# add current file directory to sys.path
-import sys
-import os
-current_dir = os.path.dirname(os.path.abspath(__file__))
-if current_dir not in sys.path:
-    sys.path.append(current_dir)
-
-from .utils import get_image_net_single_image, get_image_net_transform
+from utils import get_image_net_single_image, get_image_net_transform
 
 
 # Load a pre-trained model (e.g., ResNet18)
